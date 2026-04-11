3a6024b7b97589e3c72aec44a7f6fc2246b35e5c
Karan Sharma <karansharma2302578@gmail.com>
2026-04-11 23:57:55 +0530

    conttoller,datbae,models

---
+++ backend/controllers/userController.js
@@ -0,0 +1,37 @@
+import { User } from "../models/userModel.js";
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "ALL filed are requird",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User register successfully",
+      data: newUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import 'dotenv/config';
+import connectDB from './database/db.js';
+import userRoute from './routes/userRoute.js';
+import dns from 'dns';
+dns.setServers(["8.8.8.8"]);
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
+app.use("/user",userRoute);
+
+// http://localhost:5000/user/register
+
 app.listen(PORT, () => {
+    connectDB()
     console.log(`Server is running on port ${PORT}`);
 });
